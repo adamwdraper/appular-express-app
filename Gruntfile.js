a3474f39afc32434771ebfb20c448987b2412e8d
Adam Draper <adamwdraper@gmail.com>
2013-11-02 19:11:53 -0700

    add js ggrunt build settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,6 +36,98 @@ module.exports = function(grunt) {
                     node_env: 'production'
                 }
             }
+        },
+        docs: {
+            build: {
+                options: {
+                    pretty: true
+                },
+                files: {
+                    'public/js/dev/modules/docs/json/docs.json': [
+                        'public/js/dev/**/*.js'
+                    ]
+                }
+            }
+        },
+        jshint: {
+            all: [
+                'public/js/dev/modules/**/*.js',
+                'public/js/dev/plugins/**/*.js',
+                'public/js/dev/utilities/**/*.js'
+            ],
+            options: {
+                node: true,
+                browser: true,
+                curly: true,
+                devel: false,
+                eqeqeq: true,
+                eqnull: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                globals: {
+                    define: false,
+                    requirejs: false
+                },
+                strict: false
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: 'public/js/dev',
+                    dir: 'public/js/build',
+                    paths: {
+                        'modernizr': 'libraries/modernizr/modernizr-2.6.3',
+                        'jquery': 'empty:',
+                        'jqueryFunctions': 'libraries/jquery/extensions/functions',
+                        'underscore': 'libraries/underscore/underscore-1.5.0',
+                        'backbone': 'libraries/backbone/backbone-1.0.0',
+                        'moment': 'empty:',
+                        'numeral': 'empty:',
+                        'domReady': 'libraries/require/plugins/domReady',
+                        'async': 'libraries/require/plugins/async',
+                        'json': 'libraries/require/plugins/json',
+                        'text': 'libraries/require/plugins/text'
+                    },
+                    modules: [
+                        {
+                            name: 'appular',
+                            include: [
+                                'modernizr',
+                                'libraries/require/require-2.1.9',
+                                'libraries/require/config-build',
+                                'libraries/appular/appular',
+                                'jquery',
+                                'jqueryFunctions',
+                                'underscore',
+                                'backbone',
+                                'domReady',
+                                'text'
+                            ]
+                        },
+                        {
+                            name: 'modules/demo/module',
+                            exclude: [
+                                'appular'
+                            ]
+                        },
+                        {
+                            name: 'modules/user-bar/module',
+                            exclude: [
+                                'appular'
+                            ]
+                        },
+                        {
+                            name: 'modules/docs/module',
+                            exclude: [
+                                'appular'
+                            ]
+                        }
+                    ],
+                    removeCombined: true
+                }
+            }
         }
     });
 
@@ -52,7 +144,14 @@ module.exports = function(grunt) {
     ]);
     
     grunt.registerTask('production', 'Builds starts server in development environment, and watches NODE.js files for changes.', [
+        'build',
         'express:production',
         'watch'
+    ]);
+
+    grunt.registerTask('build', 'Builds hints and builds production JS, and builds js documentation json', [
+        'jshint',
+        'docs:build',
+        'requirejs'
     ]);
 };
