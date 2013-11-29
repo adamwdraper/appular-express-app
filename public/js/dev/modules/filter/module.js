fa9800096bafc7e67a1ea3ebe1bb316ec5258599
Adam Draper <adamwdraper@gmail.com>
2013-11-29 10:43:19 -0700

    select plugin

--- public/js/dev/modules/filter/module.js
+++ public/js/dev/modules/filter/module.js
@@ -6,31 +6,38 @@
     'jquery',
     'underscore',
     'backbone',
-    'text!./templates/module.html'
-], function ($, _, Backbone, template) {
+    'text!./templates/module.html',
+    'plugins/select/plugin'
+], function ($, _, Backbone, template, Select) {
     var Module = Backbone.Module.extend({
             events: {},
-            initialize: function() {},
+            initialize: function() {
+                _.bindAll(this, 'setLocation');
+            },
             render: function() {
-                this.$el.html(_.template(template, {
-                    locations: [
+                this.$el.html(_.template(template, {}));
+
+                this.plugins.select = new Select({
+                    el: '#location',
+                    options: [
                         'San Francisco, CA',
                         'Phoenix, AZ',
                         'Boston, MA',
                         'Austin, TX'
-                    ]
-                }));
+                    ],
+                    value: this.app.params.getValue('location')
+                }).render();
+                this.listenTo(this.plugins.select, 'change:value', this.setLocation);
 
                 // use stickit to bind param models to inputs
-                this.stickit(this.app.params.get('keyword'), {
-                    '#keyword': 'value'
-                });
-
-                this.stickit(this.app.params.get('location'), {
-                    '#location': 'value'
-                });
+                // this.stickit(this.app.params.get('keyword'), {
+                //     '#keyword': 'value'
+                // });
 
                 return this;
+            },
+            setLocation: function (option, value) {
+                this.app.params.setValue('location', value);
             }
         });
 
