96d07e50cc72da95c716ba7d0c9b395197b4baac
Paul Redmond <paulrredmond@gmail.com>
2014-02-13 16:12:26 -0700

    More appular library tests.

--- public/js/dev/libraries/appular/tests/tests.js
+++ public/js/dev/libraries/appular/tests/tests.js
@@ -5,18 +5,41 @@ define([
     'appular'
 ], function ($, _, Backbone, Appular) {
     describe('Appular', function () {
+
         it('should have certain properties', function () {
             assert.property(Appular, 'version');
         });
 
-        it ('It can load an appular component', function (done) {
+        it ('should have a components property', function () {
+            assert.property(Appular, 'components');
+            expect(Appular.components).to.be.an('object');
+        });
+
+        it ('should have a config property with an environment', function () {
+            assert.property(Appular, 'config');
+            expect(Appular.config).to.be.instanceOf(Object);
+            assert.property(Appular.config, 'env');
+        });
+
+        it ('Can load an appular component', function (done) {
 
             Backbone.on('appular:component:required', function (component) {
                 assert.ok(component);
+                expect(component.get('foo')).to.equal('bar');
                 done();
             });
 
             Appular.require.component('_boilerplate', { foo: 'bar' });
+        });
+
+        it ('Can load an appular app', function (done) {
+            Backbone.on('appular:app:required', function (app) {
+                assert.ok(app);
+                expect(app).to.be.an.instanceOf(Backbone.App);
+                done();
+            });
+
+            Appular.require.app('_boilerplate');
         });
     });
 });
