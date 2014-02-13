e0aa020af17ebba69e7feadc294a1174ded7fa71
Paul Redmond <paulrredmond@gmail.com>
2014-02-13 14:11:00 -0700

    Test requiring an appular component.

--- public/js/dev/apps/default/tests.js
+++ public/js/dev/apps/default/tests.js
@@ -2,14 +2,27 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'appular',
     './app'
-], function ($, _, Backbone, App) {
+], function ($, _, Backbone, Appular, App) {
     var app = new App();
 
     describe('Default App', function () {
         describe('App', function () {
-            it('Exhists', function () {
+            it('Exists', function () {
                 assert.ok(app);
+            });
+
+            it ('It can load an appular component', function (done) {
+
+                Backbone.on('appular:component:required', function (component) {
+                    assert.ok(component);
+                    expect(component.get('foo')).to.equal('bar');
+                    expect(Appular.components).to.have.property('docs');
+                    done();
+                });
+
+                Appular.require.component('docs', { foo: 'bar' });
             });
         });
     });
